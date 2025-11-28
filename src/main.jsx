a69333b3ba21c4c7ae1f1944b980a8f70476d493
João (School) <joao.silva-coutinho@laplateforme.io>
2025-11-28 15:04:46 +0100

    bla

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from "react-router";
-import App from './App.jsx'
 import Header from './components/Header.jsx';
-import Footer from './components/footer.jsx'
+import App from './App.jsx'
 import About from './components/About.jsx';
+import Footer from './components/footer.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
